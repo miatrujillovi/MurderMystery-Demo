6f997cbe3824055efd3c25fa5abb208ec6081eea
Mia Trujillo <mia.trujillo@uniat.edu.mx>
2026-03-18 07:53:09 -0600

    TriggerManager logic
    - Added Missing connections on the TriggerManager

--- Assets/Scripts/TriggerManager.cs
+++ Assets/Scripts/TriggerManager.cs
@@ -23,6 +23,46 @@ public class TriggerManager : MonoBehaviour
             case TriggerEffect.TriggerEffects.MainDoor:
                 HandleMainDoor(); 
                 break;
+
+            case TriggerEffect.TriggerEffects.NoteForDoor:
+                HandleNoteForDoor();
+                break;
+
+            case TriggerEffect.TriggerEffects.Flowerpot:
+                HandleFlowerpot();
+                break;
+
+            case TriggerEffect.TriggerEffects.MissingFlowerpotArea:
+                HandleMissingFlowerpotArea();
+                break;
+
+            case TriggerEffect.TriggerEffects.MissingFlowerpot:
+                HandleMissingFlowerpot();
+                break;
+
+            case TriggerEffect.TriggerEffects.Lawnmower:
+                HandleLawenmower();
+                break;
+
+            case TriggerEffect.TriggerEffects.LawnmowerCode:
+                HandleLawenmowerCode();
+                break;
+
+            case TriggerEffect.TriggerEffects.Note1:
+                HandleNotes(1);
+                break;
+
+            case TriggerEffect.TriggerEffects.Note2:
+                HandleNotes(2);
+                break;
+
+            case TriggerEffect.TriggerEffects.Note3:
+                HandleNotes(3);
+                break;
+
+            case TriggerEffect.TriggerEffects.Note4:
+                HandleNotes(4);
+                break;
         }
     }
 
@@ -30,12 +70,12 @@ public class TriggerManager : MonoBehaviour
 
     private void HandleMainDoor()
     {
-
+        Debug.Log("The door is locked. I need to get inside");
     }
 
     private void HandleNoteForDoor()
     {
-
+        Debug.Log("I left the keys under the flowerpot - Martha.");
     }
 
     #endregion DoorPuzzle
@@ -44,17 +84,19 @@ public class TriggerManager : MonoBehaviour
 
     private void HandleFlowerpot()
     {
-
+        Debug.Log("A flowerpot.");
+        Debug.Log("It doesn't seem to have a key under.");
     }
 
     private void HandleMissingFlowerpotArea()
     {
-
+        Debug.Log("There seems to be something missing here...");
     }
 
     private void HandleMissingFlowerpot()
     {
-
+        Debug.Log("This is completly trapped by those veins.");
+        Debug.Log("Maybe I can use that lawnmower.");
     }
 
     #endregion FlowerpotPuzzle
@@ -63,17 +105,34 @@ public class TriggerManager : MonoBehaviour
 
     private void HandleLawenmower()
     {
-
+        Debug.Log("Can only be opened with the code.");
     }
 
     private void HandleLawenmowerCode()
     {
-
+        Debug.Log("Maybe something related to the order of the code.");
     }
 
     private void HandleNotes(int _noteNumber)
     {
+        switch (_noteNumber)
+        {
+            case 1:
+                Debug.Log("Lirio = 0");
+                break;
 
+            case 2:
+                Debug.Log("Girasol = 3");
+                break;
+
+             case 3:
+                Debug.Log("Rosa = 8");
+                break;
+
+             case 4:
+                Debug.Log("Tulipan = 6");
+                break;
+        }
     }
 
     #endregion LawnmowerPuzzle
