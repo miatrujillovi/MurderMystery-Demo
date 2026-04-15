253d34e5272cf23203c750a3375b3ffd9f3a663d
Mia Trujillo <mia.trujillo@uniat.edu.mx>
2026-04-15 13:18:47 -0600

    fsdf

--- Assets/Scripts/PuzzleVariable.cs
+++ Assets/Scripts/PuzzleVariable.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class PuzzleVariable : MonoBehaviour
 {
-    public static bool doorKeys = false;
+    public static bool hasDoorKeys = false;
+    public static bool isFlowerPotInPosition = false;
+    public static bool isFlowerpotFree = false;
+    public static bool hasLawnmower = false;
 }
